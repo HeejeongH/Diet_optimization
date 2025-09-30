f2d4a5e24300f22b931b6c8e986b3fffeb2bfdfa
HeejeongH <133004819+HeejeongH@users.noreply.github.com>
2025-09-30 22:30:09 +0900

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1210,7 +1210,7 @@ else:
                                         st.info("💡 관리자에게 문의해주세요.")
 
                     with col3:
-                        st.link_button('사용성 평가', 'https://docs.google.com/forms/d/e/1FAIpQLScM4wJH1PmNhNAgaOvtawqvQ3KgOQMXEjErl7KzodSOJGfU4w/viewform')
+                        st.link_button('사용성 평가', 'https://docs.google.com/forms/d/e/1FAIpQLScM4wJH1PmNhNAgaOvtawqvQ3KgOQMXEjErl7KzodSOJGfU4w/viewform', use_container_width=True)
                         st.caption('5주차에만 진행해주세요!')
             else:
                 st.info("최적화 시간 정보가 누락되었습니다.")
